114876f530b049dcfadb2327bf4e9f46fe9c6c41
DeograciousAggrey <deograciousaggrey@gmail.com>
2022-12-15 05:14:28 +0300

    Updated code files

--- components/section/app-footer.vue
+++ components/section/app-footer.vue
@@ -6,36 +6,26 @@
       <div class="flex items-start space-x-2">
         <TelegramIcon class="w-4 h-auto" />
         <div class="flex flex-col text-sm leading-none">
-          <span class="mb-1">Телеграм</span>
-          <a href="https://t.me/ez_cash_gg" target="_blank">
-            <strong>@ez_cash_gg</strong>
+          <span class="mb-1">Telegram</span>
+          <a href="https://t.me/" target="_blank">
+            <strong>@waipesa</strong>
           </a>
         </div>
       </div>
-      <div class="flex items-start space-x-2">
-        <VkIcon class="w-4 h-auto" />
-        <div class="flex flex-col text-sm leading-none">
-          <span class="mb-1">ВКонтакте</span>
-          <a href="https://vk.com/ezcash_official" target="_blank">
-            <strong>ezcash_official</strong>
-          </a>
-        </div>
-      </div>
+
       <div class="flex items-start space-x-2">
         <EmailIcon class="w-4 h-auto" />
         <div class="flex flex-col text-sm leading-none">
           <span class="mb-1">Email</span>
-          <a href="mailto:help@ezcash.gg" target="_blank">
-            <strong>help@ezcash.gg</strong>
+          <a href="mailto:help@waipesa.com" target="_blank">
+            <strong>help@waipesa.com</strong>
           </a>
         </div>
       </div>
       <div class="flex flex-col space-y-1 leading-none">
-        <a href="#" class="font-semibold">Пользовательское соглашение</a>
-        <a href="#" class="font-semibold">Политика конфиденциальности</a>
-        <nuxt-link :to="{ name: 'help' }" class="font-semibold"
-          >Помощь</nuxt-link
-        >
+        <a href="#" class="font-semibold">Terms of use</a>
+        <a href="#" class="font-semibold">Privacy Policy</a>
+        <nuxt-link :to="{ name: 'help' }" class="font-semibold">Help</nuxt-link>
       </div>
     </div>
   </footer>
@@ -43,11 +33,11 @@
 
 <script>
 import TelegramIcon from '@/static/img/icon/social/telegram.svg?inline'
-import VkIcon from '@/static/img/icon/social/vk.svg?inline'
+
 import EmailIcon from '@/static/img/icon/email.svg?inline'
 
 export default {
-  components: { TelegramIcon, VkIcon, EmailIcon },
+  components: { TelegramIcon, EmailIcon },
 }
 </script>
 
